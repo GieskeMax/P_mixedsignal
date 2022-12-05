2a5e7bb64870a15345e24d80770b93a509c5f5c5
MaxGieske <max.gieske01@gmail.com>
2022-12-05 20:13:58 +0100

    1.0 CMOS

--- CMOS.py
+++ CMOS.py
@@ -2,7 +2,7 @@ import math
 
 # Vorgaben
 v_dd = 1.65
-v_ss = 3.3 - v_dd
+v_ss = -1.65
 i_q = 20e-6
 u_offs = 10e-6
 u_e_max = 1.55
@@ -48,42 +48,69 @@ u_t = 25.86  # Temperaturspannung
 
 
 
-def i_d_active(beta, u_gs, u_th, u_ds):
+def calc_i_d_active(beta, u_gs, u_th, u_ds):
     return beta * ((u_gs - u_th) * u_ds - 0.5 * u_ds ** 2)
 
-def i_d_sat(beta, u_gs, u_th, u_ds):
+def calc_i_d_sat(beta, u_gs, u_th, u_ds):
     return 0.5 * beta * (u_gs - u_th) ** 2
 
-def g_m(beta, i_da):
+def calc_(beta, i_da):
     return math.sqrt(2 * beta * i_da)
 
-def g_mb(eta, g_m):
+def calc_g_mb(eta, g_m):
     return eta * g_m
 
-def g_ds(i_da, k_l, delta_l, l_eff):
+def calc_g_ds(i_da, k_l, delta_l, l_eff):
     return i_da * k_l / (2 * delta_l * (l_eff - delta_l))
 
-def u_ds_sat(u_gs, u_th):
-    return u_gs * u_th
+def calc_u_ds_sat(u_gs, u_th):
+    return u_gs - u_th
 
-def beta_n(w_eff, l_delta_l):
+def calc_beta_n(w_eff, l_delta_l):
     return mu_0n * c_ox * w_eff / l_delta_l
 
-def beta_p(w_eff, l_delta_l):
+def calc_beta_p(w_eff, l_delta_l):
     return mu_0p * c_ox * w_eff / l_delta_l
 
-def c_ox(t_ox):
+def calc_c_ox(t_ox):
     return epsilon_0 * epsilon_r_sio2 / t_ox
 
-def u_th_n(u_bs):
+def calc_u_th_n(u_bs):
+    return u_th0n + gamma * (math.sqrt(phi - u_bs) - math.sqrt(phi))
+
+def calc_u_th_p(u_bs):
     return u_th0p + gamma * (math.sqrt(phi - u_bs) - math.sqrt(phi))
 
-def u_th_p(u_bs):
-    return u_th0p + gamma * (math.sqrt(phi - u_bs) - math.sqrt(phi))
-
-def v_d (g_m1, g_m2, g_mb2, r_ds1, r_ds2, r_d):
+def calc_v_d (g_m1, g_m2, g_mb2, r_ds1, r_ds2, r_d):
     return -(g_m1*r_ds1*(1+(g_m2+g_mb2)*r_ds2)*r_d)/(2*(r_ds2+r_ds1*(1+(g_m2+g_mb2)*r_ds2)+r_d))
 
-def r_cascode (r_ds, gm, gmb):
+def calc_r_cascode (r_ds, gm, gmb):
     return r_ds*(r_ds*(gm+gmb+1/r_ds)+1)
+
+
+
+#Kaskode-Stromspiegel:
+def calc_wl_s ():
+    return 2*i_q/(k_0n*(u_a_min-v_ss-u_th0n)**2)
+
+wl_10_12 = calc_wl_s()
+print('M10, M12:', wl_10_12)
+
+def calc_u_gs_10 (wl_s):
+    return u_th0n+(math.sqrt(2*i_q/(k_0n*wl_s)))
+
+u_gs_10 = calc_u_gs_10(wl_10_12)
+print('U_gs10:',u_gs_10)
+
+# Annahme: u_gs >= u_th -→ u_bs <= -u_th damit M10 in Sättigung ist
+u_thk_11 = calc_u_th_n(-u_th0n)
+print('u_thk_11:', u_thk_11)
+
+def calc_wl_c (wl_s, u_gs, u_thk):
+    return 2*i_q/(k_0n*(u_a_min-v_ss-u_gs-u_thk)**2)
+
+wl_11_13 = calc_wl_c(wl_10_12, u_gs_10, u_thk_11)
+print('M11, M13:', wl_11_13)
+
+
 
